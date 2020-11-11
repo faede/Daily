ff38cf161104c3f67e0fd2294a5056b28c97dbec
y2z <45329467+faede@users.noreply.github.com>
2020-11-11 23:18:49 +0800

    update

---
+++ cf/csp202009-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+typedef struct Point
+{
+	int x;
+	int y;
+}Point;
+int n;
+
+int main()
+{
+
+	cin>>n;
+	Point x,a,b,c,t;
+	cin>>x.x>>x.y;
+	for(int i = 0; i < n; ++i)
+	{
+		
+
+	}
+}
