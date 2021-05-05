90018f4a27812d36ee2118a60bd3cccf6334c485
y2z <45329467+faede@users.noreply.github.com>
2021-05-05 12:21:23 +0800

    ud

--- Fin_Classify.py
+++ Fin_Classify.py
@@ -24,39 +24,39 @@ data = pd.read_csv(fname, encoding='utf-8')
 X = data.iloc[:,4:23]
 Y = data.iloc[:,23]
 
-X_train,X_test,Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
-sc=StandardScaler()
+X_train,X_test,Y_train, Y_test = train_test_split(X, Y, test_size = 0.3, random_state=0)
+sc = StandardScaler()
 sc.fit(X)
 
 standard_train = sc.transform(X_train)
 standard_test = sc.transform(X_test)
 
 
-SVMClassifier=SVC(kernel="linear",decision_function_shape='ovo',gamma='auto')
-SVMClassifier.fit(standard_train,Y_train)
-result=SVMClassifier.predict(standard_test)
+SVMClassifier = SVC(kernel = "linear", decision_function_shape = 'ovo',gamma = 'auto')
+SVMClassifier.fit(standard_train, Y_train)
+result = SVMClassifier.predict(standard_test)
 print("线性分类器：")
 #print(list(Y_test))
 #print(list(result))
 print(SVMClassifier.score(standard_test,Y_test))
 
 
-SVMClassifier=SVC(kernel="poly",degree=3,decision_function_shape='ovo',gamma='auto')
-SVMClassifier.fit(standard_train,Y_train)
-result=SVMClassifier.predict(standard_test)
+SVMClassifier = SVC(kernel="poly", degree = 3, decision_function_shape = 'ovo',gamma = 'auto')
+SVMClassifier.fit(standard_train, Y_train)
+result = SVMClassifier.predict(standard_test)
 print("多项式核函数非线性分类器：")
 #print(list(Y_test))
 #print(list(result))
 print(SVMClassifier.score(standard_test,Y_test))
 
 
-SVMClassifier=SVC(kernel="rbf",decision_function_shape='ovo',gamma='auto')
-SVMClassifier.fit(standard_train,Y_train)
-result=SVMClassifier.predict(standard_test)
+SVMClassifier = SVC(kernel="rbf", decision_function_shape = 'ovo', gamma = 'auto')
+SVMClassifier.fit(standard_train, Y_train)
+result = SVMClassifier.predict(standard_test)
 print("高斯核函数非线性分类器：")
 #print(list(Y_test))
 #print(list(result))
-print(SVMClassifier.score(standard_test,Y_test))
+print(SVMClassifier.score(standard_test, Y_test))
 
 
 
